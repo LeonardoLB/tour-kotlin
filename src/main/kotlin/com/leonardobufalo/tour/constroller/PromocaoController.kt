79925b98d9874b1a291c40fc807ac43c99ecd6e3
LeonardoLB <leonardolsbufalo@gmail.com>
2020-05-18 16:45:41 -0300

    controllers

--- src/main/kotlin/com/leonardobufalo/tour/constroller/PromocaoController.kt
+++ src/main/kotlin/com/leonardobufalo/tour/constroller/PromocaoController.kt
@@ -2,10 +2,7 @@ package com.leonardobufalo.tour.constroller
 
 import com.leonardobufalo.tour.model.Promocao
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.concurrent.ConcurrentHashMap
 
 @RestController
@@ -14,20 +11,35 @@ class PromocaoController {
     @Autowired
     lateinit var promocoes: ConcurrentHashMap<Long, Promocao>
 
-    @RequestMapping(value = ["/sayhello"], method = [RequestMethod.GET])
-    fun SayHello(): String {
-        println("Hi Charlie")
-        return "Hi Charlie"
-    }
 
     @RequestMapping(value = ["/promocoes"], method = [RequestMethod.GET])
-    fun getAllPromocoes(): ConcurrentHashMap<Long, Promocao> {
+    fun getAll(): ConcurrentHashMap<Long, Promocao> {
         return promocoes
     }
 
     @RequestMapping(value = ["/promocoes/{id}"], method = [RequestMethod.GET])
-    fun getPromocao(@PathVariable id: Long): Promocao? {
+    fun getById(@PathVariable id: Long): Promocao? {
         return promocoes[id]
+    }
+
+    @RequestMapping(value = ["/promocoes"], method = [RequestMethod.POST])
+    fun createPromocao(@RequestBody promocao: Promocao): Promocao {
+        promocoes[promocao.id] = promocao
+        return promocao
+    }
+
+    @RequestMapping(value = ["/promocoes/{id}"], method = [RequestMethod.DELETE])
+    fun delete(@PathVariable id: Long): Promocao? {
+        val PromocaoRemoved: Promocao? = promocoes[id]
+        promocoes.remove(id)
+        return PromocaoRemoved
+    }
+
+    @RequestMapping(value = ["/promocoes"], method = [RequestMethod.PUT])
+    fun update(@RequestBody promocao: Promocao): Promocao? {
+        promocoes.remove(promocao.id)
+        promocoes[promocao.id] = promocao
+        return promocoes[promocao.id]
     }
 
 }
